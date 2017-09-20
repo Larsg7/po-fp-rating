7a49fc0d7d0d5a90f6ac0a3c11ecb2137c88ff9b
Lars Gröber <larsgroeber7@gmail.com>
2017-09-20 22:46:54 +0200

    made get accessable

--- api.php
+++ api.php
@@ -8,16 +8,16 @@ const PASSPHRASE = "eeec021898eec14f9c7c888c5899a81ad4dbf1ae";
 
 $passphrase = $_POST['passphrase'];
 
+$io = new IO(RATING_FILE);
 if ($passphrase == PASSPHRASE)
 {
-    $io = new IO(RATING_FILE);
     if (isset($_POST['stars']))
     {
         $stars = $_POST['stars'];
         $feedback = $_POST['feedback'];
         $io->writeRating($stars, $feedback);
         echo "SUCCESS";
-    } else {
-        echo $io->readRatings();
     }
+} else {
+    echo $io->readRatings();
 }
