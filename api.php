990e322855158901b06bd098e9b3bb1da482c5fb
Lars Gröber <larsgroeber7@gmail.com>
2017-09-20 23:00:36 +0200

    added CORS3

--- api.php
+++ api.php
@@ -1,7 +1,10 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
 require "./IO.php";
 
